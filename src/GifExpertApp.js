2c99a36852d52daabd1a9ad25155cbad3af06a96
Oscar Martinez <oscaralfonsocamachomartinez97@gmail.com>
2022-01-28 19:18:55 -0700

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+// import PropTypes from 'prop-types';
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     setCategories( cats => [ ...cats, 'HunterXHunter' ] );
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategories={ setCategories } />
+
+            <hr/>
+
+            <ol>
+                { 
+                    categories.map( (category, i) => (
+                         <GifGrid 
+                            key={ category }
+                            category={ category } 
+                         />
+                        ))
+                }
+            </ol>
+        </>
+    );
+
+}
+
+export default GifExpertApp;
